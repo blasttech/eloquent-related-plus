beb4e83e5f7375d9a56d9a8d191823d70d9eccd2
Michael Blair <mhkblair@gmail.com>
2018-02-06 12:46:13 +1100

    Created HelperMethodTrait and moved generic functions from RelatedPlusTrait to it

---
+++ src/HelperMethodTrait.php
@@ -0,0 +1,153 @@
+<?php
+
+namespace Blasttech\EloquentRelatedPlus;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Relation;
+
+trait HelperMethodTrait
+{
+
+    /**
+     * Get the relations from a relation name
+     * $relationName can be a single relation
+     * Usage for User model:
+     * parseRelationNames('customer') returns [$user->customer()]
+     * parseRelationNames('customer.contact') returns [$user->customer(), $user->customer->contact()]
+     *
+     * @param string $relationName
+     * @return Relation[]
+     */
+    private function parseRelationNames($relationName)
+    {
+        $relationNames = explode('.', $relationName);
+        $parentRelationName = null;
+        $relations = [];
+
+        foreach ($relationNames as $relationName) {
+            if (is_null($parentRelationName)) {
+                $relations[] = $this->$relationName();
+                $parentRelationName = $this->$relationName()->getRelated();
+            } else {
+                $relations[] = $parentRelationName->$relationName();
+            }
+        }
+
+        return $relations;
+    }
+
+    /**
+     * Add backticks to a table/column
+     *
+     * @param string $column
+     * @return string
+     */
+    private function addBackticks($column)
+    {
+        return preg_match('/^[0-9a-zA-Z\.]*$/', $column) ?
+            '`' . str_replace(['`', '.'], ['', '`.`'], $column) . '`' : $column;
+    }
+
+    /**
+     * Return the sql for a query with the bindings replaced with the binding values
+     *
+     * @param Builder $builder
+     * @return string
+     */
+    private function toSqlWithBindings(Builder $builder)
+    {
+        $replacements = array_map('addslashes', $builder->getBindings());
+        $sql = $builder->toSql();
+
+        return preg_replace_callback(
+            '/(\?)(?=(?:[^\'"]|["\'][^\'"]*["\'])*$)/',
+            function () use (&$replacements) {
+                return array_shift($replacements);
+            },
+            $sql
+        );
+    }
+
+    /**
+     * Add table name to column name if table name not already included in column name
+     *
+     * @param string $table
+     * @param string $column
+     * @return string
+     */
+    private function columnWithTableName($table, $column)
+    {
+        return (preg_match('/(' . $table . '\.|`' . $table . '`)/i', $column) > 0 ? '' : $table . '.') . $column;
+    }
+
+    /**
+     * Remove a global scope if it exists
+     *
+     * @param Builder $query
+     * @param string $scopeName
+     * @return Builder
+     */
+    private function removeGlobalScope($query, $scopeName)
+    {
+        /** @var Model $this */
+        $globalScopes = $this->getGlobalScopes();
+        if (isset($globalScopes[$scopeName])) {
+            $query->withoutGlobalScope($scopeName);
+        }
+
+        return $query;
+    }
+
+    /**
+     * Check if this model has already been joined to a table or relation
+     *
+     * @param Builder $builder
+     * @param string $table
+     * @param \Illuminate\Database\Eloquent\Relations\Relation $relation
+     * @return bool
+     */
+    private function hasJoin(Builder $builder, $table, $relation)
+    {
+        if (!$this->isJoined($builder, $table)) {
+            return $this->isEagerLoaded($builder, $relation);
+        } else {
+            return true;
+        }
+    }
+
+    /**
+     * Check if model is currently joined to $table
+     *
+     * @param Builder $builder
+     * @param string $table
+     * @return bool
+     */
+    private function isJoined(Builder $builder, $table)
+    {
+        $joins = $builder->getQuery()->joins;
+        if (!is_null($joins)) {
+            foreach ($joins as $joinClause) {
+                if ($joinClause->table == $table) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Check if relation exists in eager loads
+     *
+     * @param Builder $builder
+     * @param \Illuminate\Database\Eloquent\Relations\Relation $relation
+     * @return bool
+     */
+    private function isEagerLoaded(Builder $builder, $relation)
+    {
+        $eagerLoads = $builder->getEagerLoads();
+
+        return !is_null($eagerLoads) && in_array($relation, $eagerLoads);
+    }
+}
--- src/RelatedPlusTrait.php
+++ src/RelatedPlusTrait.php
@@ -27,6 +27,8 @@ use InvalidArgumentException;
  */
 trait RelatedPlusTrait
 {
+    use HelperMethodTrait;
+
     /**
      * Boot method for trait
      *
@@ -92,34 +94,6 @@ trait RelatedPlusTrait
         }
 
         return $query;
-    }
-
-    /**
-     * Get the relations from a relation name
-     * $relationName can be a single relation
-     * Usage for User model:
-     * parseRelationNames('customer') returns [$user->customer()]
-     * parseRelationNames('customer.contact') returns [$user->customer(), $user->customer->contact()]
-     *
-     * @param string $relationName
-     * @return Relation[]
-     */
-    protected function parseRelationNames($relationName)
-    {
-        $relationNames = explode('.', $relationName);
-        $parentRelationName = null;
-        $relations = [];
-
-        foreach ($relationNames as $relationName) {
-            if (is_null($parentRelationName)) {
-                $relations[] = $this->$relationName();
-                $parentRelationName = $this->$relationName()->getRelated();
-            } else {
-                $relations[] = $parentRelationName->$relationName();
-            }
-        }
-
-        return $relations;
     }
 
     /**
@@ -265,38 +239,6 @@ trait RelatedPlusTrait
     }
 
     /**
-     * Add backticks to a table/column
-     *
-     * @param string $column
-     * @return string
-     */
-    private function addBackticks($column)
-    {
-        return preg_match('/^[0-9a-zA-Z\.]*$/', $column) ?
-            '`' . str_replace(['`', '.'], ['', '`.`'], $column) . '`' : $column;
-    }
-
-    /**
-     * Return the sql for a query with the bindings replaced with the binding values
-     *
-     * @param Builder $builder
-     * @return string
-     */
-    private function toSqlWithBindings(Builder $builder)
-    {
-        $replacements = array_map('addslashes', $builder->getBindings());
-        $sql = $builder->toSql();
-
-        return preg_replace_callback(
-            '/(\?)(?=(?:[^\'"]|["\'][^\'"]*["\'])*$)/',
-            function ($matches) use (&$replacements) {
-                return array_shift($replacements);
-            },
-            $sql
-        );
-    }
-
-    /**
      * Join a HasMany Relation
      *
      * @param Relation $relation
@@ -354,18 +296,6 @@ trait RelatedPlusTrait
         }
 
         return $builder;
-    }
-
-    /**
-     * Add table name to column name if table name not already included in column name
-     *
-     * @param string $table
-     * @param string $column
-     * @return string
-     */
-    private function columnWithTableName($table, $column)
-    {
-        return (preg_match('/(' . $table . '\.|`' . $table . '`)/i', $column) > 0 ? '' : $table . '.') . $column;
     }
 
     /**
@@ -459,24 +389,6 @@ trait RelatedPlusTrait
     }
 
     /**
-     * Remove a global scope if it exists
-     *
-     * @param Builder $query
-     * @param string $scopeName
-     * @return Builder
-     */
-    private function removeGlobalScope($query, $scopeName)
-    {
-        /** @var Model $this */
-        $globalScopes = $this->getGlobalScopes();
-        if (isset($globalScopes[$scopeName])) {
-            $query->withoutGlobalScope($scopeName);
-        }
-
-        return $query;
-    }
-
-    /**
      * Check if column being sorted by is from a related model
      *
      * @param Builder $query
@@ -486,6 +398,7 @@ trait RelatedPlusTrait
      */
     public function scopeOrderByCheckModel(Builder $query, $column, $direction)
     {
+        /** @var Model $query */
         $query->orderBy(DB::raw($column), $direction);
 
         $periodPos = strpos($column, '.');
@@ -520,58 +433,6 @@ trait RelatedPlusTrait
         }
 
         return $query;
-    }
-
-    /**
-     * Check if this model has already been joined to a table or relation
-     *
-     * @param Builder $builder
-     * @param string $table
-     * @param \Illuminate\Database\Eloquent\Relations\Relation $relation
-     * @return bool
-     */
-    protected function hasJoin(Builder $builder, $table, $relation)
-    {
-        if (!$this->isJoined($builder, $table)) {
-            return $this->isEagerLoaded($builder, $relation);
-        } else {
-            return true;
-        }
-    }
-
-    /**
-     * Check if model is currently joined to $table
-     *
-     * @param Builder $builder
-     * @param string $table
-     * @return bool
-     */
-    private function isJoined(Builder $builder, $table)
-    {
-        $joins = $builder->getQuery()->joins;
-        if (!is_null($joins)) {
-            foreach ($joins as $joinClause) {
-                if ($joinClause->table == $table) {
-                    return true;
-                }
-            }
-        }
-
-        return false;
-    }
-
-    /**
-     * Check if relation exists in eager loads
-     *
-     * @param Builder $builder
-     * @param \Illuminate\Database\Eloquent\Relations\Relation $relation
-     * @return bool
-     */
-    private function isEagerLoaded(Builder $builder, $relation)
-    {
-        $eagerLoads = $builder->getEagerLoads();
-
-        return !is_null($eagerLoads) && in_array($relation, $eagerLoads);
     }
 
     /**
